ee58307b0c5241969e2f5bbacaacf4f9cda7f48f
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-27 16:43:24 -0500

    js-menu closes on anchor link

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,13 @@
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
+let anchors = document.querySelectorAll('.nav-link')
 
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
 
-ScrollReveal().reveal('.h-reveal');
+let length = anchors.length;
+for (let i = 0; i < length; i++) {
+  anchors[i].addEventListener('click', function () {
+    mainNav.classList.remove('active');
+  })};
